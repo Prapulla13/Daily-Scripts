71a6f0edda1a560a6fb8866f04499c6bb0a38e21
Prapulla13 <prapullaar@gmail.com>
2024-12-24 14:50:41 +0530

    Different ways of Function Declaration

---
+++ FunctionDeclaration.js
@@ -0,0 +1,15 @@
+//function Declaration
+function add(a,b){
+    console.log(a+b);
+}
+add(100,200);
+
+const sub = function(a,b){
+    console.log(a-b);
+}
+sub(200,100);
+
+const mul = (a,b)=>{
+console.log(a * b);
+}
+mul(10,20);
