3d4dc07c29664015744cfdc0c27aa666eda3b186
Prapulla13 <prapullaar@gmail.com>
2024-12-24 14:16:42 +0530

    Modified Operators file

--- Operators.js
+++ Operators.js
@@ -1,6 +1,18 @@
-console.log(10+20);
-console.log(2**3);//8
-var a = 10;
-console.log(a++);//Post
-console.log(a);// a= 11
-console.log(++a)//Pre
+//console.log(10+20);
+//console.log(2**3);//8
+//var a = 10;
+//console.log(a++);//Post
+//console.log(a);// a= 11
+//console.log(++a)//Pre
+
+console.log(10 == 10);
+console.log(10 == '10');
+
+console.log(10 === '10');
+console.log(10 == 10);
+
+console.log(true && true);
+console.log(true || false);
+console.log(!true);
+
+console.log((10 == '10' ? 'Equal by Value': 'Not Equal by Value'));
